275c0ee8eab52a68d0086f8d4c382f67de064e28
ygordantas <ygord.furtado@gmail.com>
2024-03-28 19:21:13 -0600

    1- setup a get request to retrieve the todoCategories 2- setup a post request to add a todo category

--- app.js
+++ app.js
@@ -1,12 +1,62 @@
-import { createServer } from 'node:http';
+import { createServer } from "node:http";
 
-const server = createServer((_req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end("Uhull COMIT ! Let's goooo!\n");
+const PORT = 3000;
+
+const TODO_CATEGORIES = [
+  {
+    id: 1,
+    name: "Work",
+  },
+  {
+    id: 2,
+    name: "Groceries",
+  },
+  {
+    id: 3,
+    name: "Finance",
+  },
+  {
+    id: 4,
+    name: "Family",
+  },
+  {
+    id: 5,
+    name: "Cooking",
+  },
+  {
+    id: 6,
+    name: "Gardening",
+  },
+];
+
+const server = createServer((req, res) => {
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(TODO_CATEGORIES));
+  } else if (req.method === "POST") {
+    res.writeHead(201, { "Content-Type": "application/json" });
+    let body = "";
+
+    req.on("data", (chunks) => {
+      body += chunks.toString();
+    });
+
+    req.on("end", () => {
+      const newTodo = JSON.parse(body);
+      const newTodoId = TODO_CATEGORIES[TODO_CATEGORIES.length - 1].id + 1;
+
+      TODO_CATEGORIES.push({ id: newTodoId, ...newTodo });
+
+      res.end(JSON.stringify(TODO_CATEGORIES[TODO_CATEGORIES.length - 1]));
+    });
+
+    
+  } else {
+    res.writeHead(405, { "Content-Type": "application/json" });
+    res.end("Method not allowed");
+  }
 });
 
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
 });
-
-// => request => intercom(convert your speech into an order that's display on screen) => print in printer kitchen 
